e3949e3b7a354ee65a3603f067e49305fee89127
dangfan <dangf09@gmail.com>
2019-02-12 21:06:09 +0800

    Remove padding of DES encrypt

--- src/core/operations/DESEncrypt.mjs
+++ src/core/operations/DESEncrypt.mjs
@@ -79,7 +79,7 @@ Triple DES uses a key length of 24 bytes (192 bits).`);
         const cipher = forge.cipher.createCipher("DES-" + mode, key);
         cipher.start({iv: iv});
         cipher.update(forge.util.createBuffer(input));
-        cipher.finish();
+        cipher.finish(function(){});
 
         return outputType === "Hex" ? cipher.output.toHex() : cipher.output.getBytes();
     }
